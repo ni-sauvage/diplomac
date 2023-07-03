cf9951ff6bf92f3672df45b69b14137b45e1c3a7
ni-sauvage <sauvagen@tcd.ie>
2023-07-03 12:48:56 +0100

    Added all territories to board

--- be_log/territories.py
+++ be_log/territories.py
@@ -1,10 +1,27 @@
 from units import Unit
 class Territory:
-    def __init__(self, name : str, coastal : bool, l_s: bool, occ : Unit ):
+    def __init__(self, name : str, occ : Unit ):
         self.name = name
-        self.coastal = coastal
-        self.l_s = l_s
         self.occ = occ
     
     def __str__(self) -> str:
         return self.name
+
+class Sea(Territory):
+    def __init__(self, name : str, occ : Unit):
+        super().__init__(name, occ)
+
+class Coastal(Territory):
+    def __init__(self, name : str, occ : Unit, centre: bool, hs : bool):
+        self.name = name
+        self.occ = occ
+        self.centre = centre
+        self.hs = hs
+
+class Inland(Territory):
+    def __init__(self, name : str, occ : Unit, centre: bool, hs : bool):
+        self.name = name
+        self.occ = occ
+        self.centre = centre
+        self.hs = hs
+
--- be_log/units.py
+++ be_log/units.py
@@ -1,11 +1,12 @@
+import networkx as nx
 class Unit:
-    def __init__(self, country : str):
+    def __init__(self, country : str, pos : str):
         self.country = country
 
 class Fleet(Unit):
-    def __init__(self, country : str):
-        super(Unit, self).__init__(self, country)
+    def __init__(self, country : str, pos: str):
+        super(Unit, self).__init__(self, country, pos)
 
 class Army(Unit):
-    def __init__(self, country : str):
-        super(Unit, self).__init__(self, country)
+    def __init__(self, country : str, pos: str):
+        super(Unit, self).__init__(self, country, pos)
